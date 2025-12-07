07bbd3b75f8ff72487d60458ed0f039685a15079
etooley77 <tooleye77@gmail.com>
2025-12-07 00:05:14 -0500

    Reformat game functions, update tie mechanics, and outline AI opponent class

---
+++ ai.py
@@ -0,0 +1,25 @@
+import pygame
+
+from constants import *
+
+from player import Player
+
+# 
+class AIPlayer(Player):
+	def __init__(self, color):
+		super().__init__(color)
+
+	
+
+	def check_board(self):
+		pass
+
+	def check_player_pieces(self):
+		pass
+
+	def check_self_pieces(self):
+		pass
+
+	# AI turn
+	def make_move(self):
+		pass
